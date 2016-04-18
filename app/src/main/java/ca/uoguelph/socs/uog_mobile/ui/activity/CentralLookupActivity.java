4d005ce3d276e78deb596786a4ebbd0b9e82c233
Julian Horvat <julian.m.horvat@gmail.com>
2016-04-18 12:26:15 -0400

    properly handle failed lookup

--- app/src/main/java/ca/uoguelph/socs/uog_mobile/ui/activity/CentralLookupActivity.java
+++ app/src/main/java/ca/uoguelph/socs/uog_mobile/ui/activity/CentralLookupActivity.java
@@ -1,7 +1,6 @@
 package ca.uoguelph.socs.uog_mobile.ui.activity;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -9,6 +8,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import ca.uoguelph.socs.uog_mobile.R;
@@ -17,7 +17,6 @@ import ca.uoguelph.socs.uog_mobile.injection.component.CentralLookupComponent;
 import ca.uoguelph.socs.uog_mobile.injection.component.DaggerCentralLookupComponent;
 import ca.uoguelph.socs.uog_mobile.injection.module.CentralLookupModule;
 import ca.uoguelph.socs.uog_mobile.util.RxUtils;
-import com.google.gson.Gson;
 import javax.inject.Inject;
 import rx.Subscription;
 import timber.log.Timber;
@@ -58,7 +57,13 @@ public class CentralLookupActivity extends BaseActivity {
                                              Intent in = new Intent(this, WebAdvisorActivity.class);
                                              in.putExtra("user", user);
                                              startActivity(in);
-                                         }, error -> Timber.e(error.getMessage()));
+                                         }, error -> {
+                                             Timber.e(error.getMessage());
+                                             Toast.makeText(this,
+                                                            "Lookup failed, did you mistype?",
+                                                            Toast.LENGTH_LONG).show();
+                                             loading.setVisibility(View.GONE);
+                                         });
             }
         });
     }
